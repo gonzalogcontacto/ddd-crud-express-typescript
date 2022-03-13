44356ab3a546dd82fa26ae945d50aa40c27006d6
gonzalogcontacto <gonzalogcontacto@gmail.com>
2022-03-13 19:58:04 +0100

    refactoring to shared context

--- src/core/event/domain/DomainEventSubscriber.ts
+++
@@ -1,3 +0,0 @@
-export interface DomainEventSubscriber {
-  subscribedTo(): Array<String>;
-}
--- src/core/event/domain/EventBus.ts
+++
@@ -1,4 +0,0 @@
-import { DomainEvent } from "./DomainEvent";
-export interface EventBus {
-  publish(events: DomainEvent): void;
-}
