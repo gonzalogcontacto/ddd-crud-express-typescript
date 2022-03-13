ee25a0590dfd8bf7f9df301b38a41555098a5d49
gonzalogcontacto <gonzalogcontacto@gmail.com>
2022-03-13 19:11:36 +0100

    imlpement MockEventBus

--- src/core/user/UserServiceProvider.ts
+++ src/core/user/UserServiceProvider.ts
@@ -2,11 +2,14 @@ import { CreateUserHandler } from "./application/create/CreateUserHandler";
 import { UserCreator } from "./application/create/UserCreator";
 import { MongooseCourseRepository } from "./infra/MoongoseUserRepository";
 
+class MockEventBus {
+  publish() {}
+}
 export class UserServiceProvider {
   commandHandlers() {
     return {
       CreateUserHandler: new CreateUserHandler(
-        new UserCreator(new MongooseCourseRepository())
+        new UserCreator(new MongooseCourseRepository(), new MockEventBus())
       ),
     };
   }
