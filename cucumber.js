3f5217c98adbae372a1bb376e5d5abc36e2c632d
gonzalogcontacto <gonzalogcontacto@gmail.com>
2022-11-03 02:50:50 +0100

    jest and cucumber inital setup

---
+++ cucumber.js
@@ -0,0 +1,15 @@
+const common = [
+  "--require-module ts-node/register", // Load TypeScript module
+];
+
+const api_backend = [
+  ...common,
+  "tests/apps/api/features/**/*.feature",
+  "--require tests/apps/api/features/step_definitions/*.steps.ts",
+  "--format progress-bar", // Load custom formatter
+  "--format node_modules/cucumber-pretty", // Load custom formatter
+].join(" ");
+
+module.exports = {
+  default: api_backend,
+};
