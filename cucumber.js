dcdadda359d5bbac3eb6ab0c3c80d200a3ce5f57
gonzalogcontacto <gonzalogcontacto@gmail.com>
2022-11-03 03:28:00 +0100

    Skip pretty format, is bringing error

--- cucumber.js
+++ cucumber.js
@@ -7,7 +7,7 @@ const api_backend = [
   "tests/apps/api/features/**/*.feature",
   "--require tests/apps/api/features/step_definitions/*.steps.ts",
   "--format progress-bar", // Load custom formatter
-  "--format node_modules/cucumber-pretty", // Load custom formatter
+  //"--format  node_modules/cucumber-pretty", // Load custom formatter
 ].join(" ");
 
 module.exports = {
