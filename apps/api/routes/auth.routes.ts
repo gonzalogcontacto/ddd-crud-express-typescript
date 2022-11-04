f76ba08fd7da0f3b23fe9164d8643db729522a5c
Gonzalo García <gonzalo.navarro@ft.com>
2022-11-04 20:55:27 +0100

    removed legacy controller and routing

--- apps/api/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { authController } from "../controllers/auth.controller";
-
-const router = Router();
-
-//Login route
-router.post("/login", authController.login);
-
-export default router;
